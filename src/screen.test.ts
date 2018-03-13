493b4c59267f1329fb0ec82e9fe778c0431f53af
Julien Giovaresco <dev@giovaresco.fr>
2018-03-13 13:56:08 +0100

    display 3 4 5 6 7 8 9

--- src/screen.test.ts
+++ src/screen.test.ts
@@ -24,4 +24,60 @@ describe('display', () => {
 |_ `,
     );
   });
+
+  it('displays 3', () => {
+    expect(display(3)).toBe(
+      ` _ 
+ _|
+ _|`,
+    );
+  });
+
+  it('displays 4', () => {
+    expect(display(4)).toBe(
+      `   
+|_|
+  |`,
+    );
+  });
+
+  it('displays 5', () => {
+    expect(display(5)).toBe(
+      ` _ 
+|_ 
+ _|`,
+    );
+  });
+
+  it('displays 6', () => {
+    expect(display(6)).toBe(
+      ` _ 
+|_ 
+|_|`,
+    );
+  });
+
+  it('displays 7', () => {
+    expect(display(7)).toBe(
+      ` _ 
+  |
+  |`,
+    );
+  });
+
+  it('displays 8', () => {
+    expect(display(8)).toBe(
+      ` _ 
+|_|
+|_|`,
+    );
+  });
+
+  it('displays 9', () => {
+    expect(display(9)).toBe(
+      ` _ 
+|_|
+  |`,
+    );
+  });
 });
--- src/screen.ts
+++ src/screen.ts
@@ -10,7 +10,35 @@ const TWO = ` _
  _|
 |_ `;
 
-const digits = [ZERO, ONE, TWO];
+const THREE = ` _ 
+ _|
+ _|`;
+
+const FOUR = `   
+|_|
+  |`;
+
+const FIVE = ` _ 
+|_ 
+ _|`;
+
+const SIX = ` _ 
+|_ 
+|_|`;
+
+const SEVEN = ` _ 
+  |
+  |`;
+
+const EIGHT = ` _ 
+|_|
+|_|`;
+
+const NINE = ` _ 
+|_|
+  |`;
+
+const digits = [ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE];
 
 export function display(digit: number): string {
   return digits[digit];
